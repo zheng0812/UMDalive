4c5cf390c792e06df4e7cf5dc1b4473c373908e0
Paul Sipper <sippe041@d.umn.edu>
2018-03-29 09:59:49 -0500

    Removed unused imports

--- umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/AllClubs.java
+++ umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/AllClubs.java
@@ -2,16 +2,12 @@ package com.example.cs4532.umdalive;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.Button;
 
 public class AllClubs implements View.OnClickListener{
 
     private Activity activity;
-
     private Context context;
-
     private RestCalls rest;
 
     public AllClubs (Activity a, Context c){
