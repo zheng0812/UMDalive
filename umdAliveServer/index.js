ca431a5a2ee4e373407a97359115fff5119bdd20
Dominic Cheek <cheek010@d.umn.edu>
2018-03-29 00:57:23 -0500

    Added RestAPI

---
+++ umdAliveServer/index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var queryParser = require('body-parser');
+
+// Initialize main instanced class
+var app = express();
+
+// Set the port
+app.set("port", 32892);
+
+// Support encoded bodies
+app.use(queryParser.urlencoded({
+    extended: true
+}));
+
+// Support JSON-encoded bodies
+app.use(queryParser.json());
+
+//loads the data base functions
+var dataBase = require('./dataBaseFunctions.js');
+
+//Club server Calls
+app.put('/createClub', function (req, res) {
+  if (!req.body){
+    return res.sendStatus(400);
+  }
+
+  var clubData = {
+    "name" : req.body.name,
+    "description" : req.body.description
+    "members" : req.body.memebers
+    "events" : req.body.events;
+  };
+  
+  dataBase.createClub(clubData);
+  console.log(req.body.name + " has been created.");
+});
+
+app.post('/getClub', function (req, res) {
+  if (!req.body){
+    return res.sendStatus(400);
+  }
+  dataBase.getClub(req.body.clubID, function (docs){
+    res.send(docs);
+  });
+});
+
+app.get('/getAllClubs', function (req, res) {
+  if (!req.body){
+    return res.sendStatus(400);
+  }
+  dataBase.getAllClubs(function (docs){
+    res.send(docs);
+  });
+});
+
+//User server Calls
+app.put('/createUser', function (req, res){
+  if (!req.body){
+    return res.sendStatus(400);
+  }
+
+  var userData = {
+    "name" : req.body.name,
+    "email" : req.body.email,
+    "major" : req.body.major,
+    "about" : req.body.about,
+    "clubs" : req.body.clubs
+  };
+
+  dataBase.createUser(userData);
+  console.log(req.body.name + " has been created");
+});
+
+app.listen(app.get("port"), function () {
+    console.log('umdAliveServer running on port ', app.get("port"));
+
+});
