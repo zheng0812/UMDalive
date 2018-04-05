04cbf7deaa980eaad112d6af6f5baa8d0be460f7
DevinCheek <cheek011@d.umn.edu>
2018-04-05 09:56:16 -0500

    Throws JSON Exception

--- umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/CallBack.java
+++ umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/CallBack.java
@@ -1,7 +1,8 @@
 package com.example.cs4532.umdalive;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public interface CallBack {
-    void callBack(JSONObject serverResponse);
+    void callBack(JSONObject serverResponse) throws JSONException;
 }
