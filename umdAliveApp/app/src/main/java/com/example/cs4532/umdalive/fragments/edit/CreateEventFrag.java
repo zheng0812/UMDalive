bb89a64fc301623acd76b6bb8f4f8ff716b1afce
Paul Sipper <sippe041@d.umn.edu>
2018-04-26 02:45:03 -0500

    Added javaDocs to CreateEventFrag and EditClubFrag

--- umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/fragments/edit/CreateEventFrag.java
+++ umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/fragments/edit/CreateEventFrag.java
@@ -4,5 +4,12 @@ package com.example.cs4532.umdalive.fragments.edit;
  * Created by Josh on 4/25/2018.
  */
 
+/**
+ * @author Josh Senst
+ *
+ * 4/26/2018
+ *
+ * Class that creates the Create Event Page
+ */
 public class CreateEventFrag {
 }
