2edc9192c0ea58906445a4de0e27c23326222a86
cheek011 <cheek011@d.umn.edu>
2018-04-26 06:41:12 -0500

    gtesrd

--- umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/fragments/base/ClubFrag.java
+++ umdAliveApp/app/src/main/java/com/example/cs4532/umdalive/fragments/base/ClubFrag.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,6 +19,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.cs4532.umdalive.R;
 import com.example.cs4532.umdalive.RestSingleton;
 import com.example.cs4532.umdalive.UserSingleton;
@@ -46,6 +50,7 @@ public class ClubFrag extends Fragment{
     //Layout Components
     private TextView clubName;
     private TextView clubDescription;
+    private ImageView clubImage;
     private LinearLayout members;
     private LinearLayout eventsList;
     private Button Join;
@@ -68,11 +73,11 @@ public class ClubFrag extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         leaveJoinData = new JSONObject();
-        view.setVisibility(View.GONE);
+
 
         //Create View
         view = inflater.inflate(R.layout.club_layout, container, false);
-
+        view.setVisibility(View.GONE);
         //Get Layout Components
         getLayoutComponents();
 
@@ -108,6 +113,8 @@ public class ClubFrag extends Fragment{
     private void getLayoutComponents() {
         clubName = (TextView) view.findViewById(R.id.ClubNameView);
         clubDescription = (TextView) view.findViewById(R.id.DescriptionView);
+        clubImage = view.findViewById(R.id.clubImage);
+
         members = (LinearLayout) view.findViewById(R.id.MembersLayout);
         eventsList = (LinearLayout) view.findViewById(R.id.EventsLayout);
         Join = (Button) view.findViewById(R.id.ClubJoin);
@@ -200,34 +207,59 @@ public class ClubFrag extends Fragment{
      */
     private void updateUI(JSONObject res) throws JSONException{
         view.setVisibility(View.VISIBLE);
-        clubData = res;
-        Boolean found = false;
-        String userID = UserSingleton.getInstance().getUserID();
         getActivity().findViewById(R.id.PageLoading).setVisibility(View.GONE);
-        clubName.setText(res.getString("name"));
-        clubName.setTag(res.getString("_id"));
-        clubDescription.setText(res.getString("description"));
-        JSONObject memberJson = res.getJSONObject("members");
+
+        clubData = res;
+
+        if (clubData.get("profilePicture") == null) {
+            Glide.with(this)
+                    .load(UserSingleton.getInstance().getProfileUrl())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(clubImage);
+        } else {
+            Glide.with(this)
+                    .load("http://cdn3.radioflyer.com/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/c/l/classic-red-wagon-model-18_1_3_1.jpg")
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(clubImage);
+        }
+
+        Boolean found = false;
+
+        String userID = UserSingleton.getInstance().getUserID();
+
+        clubName.setText(clubData.getString("name"));
+        clubName.setTag(clubData.getString("_id"));
+        clubDescription.setText(clubData.getString("description"));
+
+        JSONObject memberJson = clubData.getJSONObject("members");
+
         JSONArray regulars = memberJson.getJSONArray("regular");
         JSONObject admins = memberJson.getJSONObject("admin");
-        JSONArray events = res.getJSONArray("events");
+
+        JSONArray events = clubData.getJSONArray("events");
+
         LinearLayout hla = new LinearLayout(view.getContext());
         hla.setOrientation(LinearLayout.HORIZONTAL);
+
         if(admins.getString("userID")!=userID){
             editClub.setVisibility(View.GONE);
             addEvent.setVisibility(view.GONE);
         }
+
         TextView memberAdmin = new TextView(view.getContext());
         TextView admin = new TextView(view.getContext());
+
         admin.setText("admin");
-        admin.setTextSize(16);
+        admin.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
         admin.setGravity(Gravity.RIGHT);
         admin.setWidth(members.getWidth()/2);
+
         memberAdmin.setText(admins.getString("name"));
         if(admins.getString("userID")==userID){
             found = true;
         }
-        memberAdmin.setTextSize(16);
+
+        memberAdmin.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
         memberAdmin.setWidth(members.getWidth()/2);
         memberAdmin.setTag(admins.getString("userID"));
         memberAdmin.setOnClickListener(new View.OnClickListener() {
@@ -241,11 +273,15 @@ public class ClubFrag extends Fragment{
                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,frag).commit();
                 }
             });
+
         hla.addView(memberAdmin);
         hla.addView(admin);
+
         members.addView(hla);
+
         for (int i=0;i<regulars.length();i++){
             String name = regulars.getJSONObject(i).getString("name");
+
             TextView member = new TextView(view.getContext());
             member.setText(name);
             member.setTextSize(16);
@@ -273,7 +309,7 @@ public class ClubFrag extends Fragment{
                 hl.setOrientation(LinearLayout.HORIZONTAL);
                 String name = events.getJSONObject(i).getString("name");
                 String day = events.getJSONObject(i).getString("date");
-                String id = events.getJSONObject(i).getString("eventID").toString();
+                String id = events.getJSONObject(i).getString("_id").toString();
                 TextView event = new TextView(view.getContext());
                 TextView date = new TextView(view.getContext());
                 date.setText(day);
